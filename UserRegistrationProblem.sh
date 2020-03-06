30b3895828a554a216912ba23001c045fe082544
Nileshlad <ladnilesh1111@gmail.com>
2020-03-06 10:24:28 +0530

    Added: Welcome User Registration Problem

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-echo "-------------------------WELCOME USER REGISTRATION SYSTEM----------------------------"
+echo "--------------------------WELCOME USER REGISTRATION SYSTEM----------------------------"
