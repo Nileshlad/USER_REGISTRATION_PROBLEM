43b4ceb27fbf38a4d78b03ae63ced1aa978bb748
Nileshlad <ladnilesh1111@gmail.com>
2020-03-06 10:30:24 +0530

    Added: To check valid first name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -x
 
 echo "--------------------------WELCOME USER REGISTRATION SYSTEM----------------------------"
+
+#TO FUNCTION  IN FIRST NAME
+function firstName () {
+	echo "Enter the first word"
+	read word
+	pattern=[a-zA-Z]{3,}
+
+	#TO CHECK THE VALID AND INVALID
+	if [[ $word =~ $pattern ]]
+	then
+		printf "valid first name"
+	else
+		printf "invalid first name"
+	fi
+}
+firstName
