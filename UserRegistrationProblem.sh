391679af1c6f7a4989663c60e02405f768d67072
Nileshlad <ladnilesh1111@gmail.com>
2020-03-06 11:01:57 +0530

    Added: To check mobile number valid

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -38,7 +38,21 @@ function email(){
 	fi
 }
 
+#TO FUNCTION MOBILE
+function mobileNumber(){
+	echo "Enter the mobile number"
+	read number
+	pattern="(0/91)?[7-9][0-9]{9}"
+	#TO CHECK THE VALID AND INVALID EMAIL
+	if [[ $number =~ $pattern ]]
+	then
+		printf "valid number"
+	else
+		printf "invalid number"
+	fi
+}
 #TO FUNCTION CALL
 firstName
 lastName
 email
+mobileNumber
